6bd93b34d381c7bffee8c059e5fae1d3c9e4e34d
GuilleClaus <guillermo.j.ojeda.developer@gmail.com>
2021-10-24 23:19:34 -0300

    Version Final 1.0

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public Vector3 direction = Vector3.forward;
+    public float damage = 5.0f;
+    public float duration = 2.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        duration -= Time.deltaTime;
+
+        transform.Translate(speed * Time.deltaTime * direction);
+
+        if (duration <= 0)
+            Destroy(gameObject);
+
         
     }
 }
--- Assets/Scripts/Canion_player.cs
+++ Assets/Scripts/Canion_player.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Canion_player : MonoBehaviour
 {
+
+    public GameObject PuntoDisparo;
+    public GameObject Bala;
+    public float lapsoDisparo = 2.0f;
+    private float TiempoRecarga;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TiempoRecarga = lapsoDisparo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TiempoRecarga -= Time.deltaTime;
+        if (TiempoRecarga <= 0)
+        {
+            Disparar();
+            TiempoRecarga = lapsoDisparo;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Bala.transform.localScale = new Vector3(Bala.transform.localScale.x * 2, Bala.transform.localScale.y * 2, Bala.transform.localScale.z * 2);
+            Debug.Log("Se duplica Tamaño de Bala");
+        }
+
+    }
+
+    void Disparar()
+    {
+        Instantiate(Bala, PuntoDisparo.transform.position, Bala.transform.rotation);
+        Debug.Log("Disparo!!!");
+
     }
 }
